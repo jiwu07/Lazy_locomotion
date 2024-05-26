a69b3da66a66665aa8aab4a41e8e5f68212da0cc
Emma0723 <jiwuuu07@gmail.com>
2024-05-26 14:03:07 +0200

    cameraMove

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-   public GameObject avatarHead; 
-   private Transform headPosition;
+    public GameObject avatarHead;
+    private Transform headPosition;
+    
 
 
     void Start(){
         headPosition = avatarHead.transform;
-    }   
-   // Update is called once per frame
+        Transform cameraTransform = Camera.main.transform;
+        transform.position = headPosition.position - cameraTransform.position;
+
+    }
+    // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(transform.position.x,headPosition.position.y, transform.position.z);
